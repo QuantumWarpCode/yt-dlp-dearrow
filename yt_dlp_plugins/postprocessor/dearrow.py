db7a14cad9b72327f76ee44aea9a841e96484175
Christopher Cullinane <127466351+topher-nullset@users.noreply.github.com>
2023-12-28 14:52:58 -0900

    feat: prints each title if multiple are present, user must enter number to continue

--- yt_dlp_plugins/postprocessor/dearrow.py
+++ yt_dlp_plugins/postprocessor/dearrow.py
@@ -15,21 +15,38 @@ class DeArrowPP(PostProcessor):
         api_data = self._download_json(
             f'https://sponsor.ajay.app/api/branding?videoID={info["id"]}') or {}
 
-        # Store the original title
-        info['original_title'] = info.get('title', '')
-        info['dearrow_title'] = 'NA'
-        
-        # Check if the title is present in the API response and update accordingly
+        # Check if titles are present in the API response
         if 'titles' in api_data and api_data['titles']:
-            new_title = api_data['titles'][0].get('title')
-            if new_title:
-                self.to_screen(f'Original title: {info["original_title"]}')
+            titles = [title.get('title') for title in api_data['titles'] if title.get('title')]
 
-                # Update the title
-                info['title'] = new_title
-                info['dearrow_title'] = new_title
+            # If multiple titles are found, let the user choose
+            if len(titles) > 1:
+                self.to_screen("Multiple titles found. Please choose one:")
+                for i, title in enumerate(titles):
+                    self.to_screen(f"{i + 1}: {title}")
+
+                # Get user's choice
+                choice = input("Enter the number of the title you want to choose: ")
+                try:
+                    # Validate user input
+                    choice = int(choice) - 1
+                    if choice < 0 or choice >= len(titles):
+                        raise ValueError
+                except ValueError:
+                    self.to_screen("Invalid selection. Using the first title.")
+                    choice = 0
+
+                new_title = titles[choice]
+            else:
+                new_title = titles[0]
+
+            # Store the original title and update with the new title
+            info['original_title'] = info.get('title', '')
+            self.to_screen(f'Original title: {info["original_title"]}')
+            info['title'] = new_title
+
         else:
             self.to_screen("No new title found in the API response.")
-            # print(api_data)     
 
         return [], info
+
