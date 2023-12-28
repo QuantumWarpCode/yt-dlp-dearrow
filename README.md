99d8eab3c9f06e290cc1872e6de70eb9d96ec6ef
E'lo <elopus001@gmail.com>
2023-12-28 04:02:06 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+A [yt-dlp](https://github.com/yt-dlp/yt-dlp) postprocessor [plugin](https://github.com/yt-dlp/yt-dlp#plugins) for [DeArrow](https://dearrow.ajay.app/).
+
+Many thanks to https://github.com/topher-nullset and the RYD plugin for some code.
+
+## Installation
+
+Requires yt-dlp `2023.01.01` or above.
+
+You can install this package with pip:
+```
+python3 -m pip install -U https://github.com/QuantumWarpCode/yt-dlp-dearrow/archive/master.zip
+```
+
+See [yt-dlp installing plugins](https://github.com/yt-dlp/yt-dlp#installing-plugins) for the many other ways this plugin package can be installed.
+
+## Usage
+
+Pass `--use-postprocessor DeArrow:when=pre_process` to activate the PostProcessor
---
+++ yt_dlp_plugins/postprocessor/dearrow.py
@@ -0,0 +1,38 @@
+from yt_dlp.postprocessor.common import PostProcessor
+
+# Update this with the actual field mappings and API URL
+DEARROW_FIELDS = {
+    'title': 'dearrow_title',  # Example mapping
+    # Add other fields as necessary
+}
+DEARROW_API_URL = 'https://sponsor.ajay.app/api/branding?videoID='  # Replace with actual API URL
+
+SUPPORTED_EXTRACTORS = {
+    'Youtube',
+}
+
+class DeArrowPP(PostProcessor):
+    def run(self, info):
+        #print(info['title'])
+        extractor = info['extractor_key']
+        if extractor not in SUPPORTED_EXTRACTORS:
+            self.to_screen(f'{self.PP_NAME} is not supported for {extractor}')
+            return [], info
+
+        # Call the DeArrow API
+        api_data = self._download_json(
+            f'https://sponsor.ajay.app/api/branding?videoID={info["id"]}') or {}
+
+        info['DEARROW'] = {
+            'response': api_data,
+            'original': {k: info.get(k) for k in DEARROW_FIELDS.keys()}
+        }
+        
+        # RYD method
+        #if api_data:
+        #    info.update({k: api_data.get(v) for k, v in DEARROW_FIELDS.items()})
+
+        info['title'] = info['DEARROW']['response']['titles'][0]['title']
+
+        #print(info['title'])
+        return [], info
